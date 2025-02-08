4088f1725410008d1874ae9f5e5d372b455623cf
Maya Lee <maya.v.lee393@gmail.com>
2025-02-08 15:22:36 -0600

    menu api

--- menu-api.py
+++ menu-api.py
@@ -1,5 +1,7 @@
 import pandas
 import requests
+import json
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
@@ -16,26 +18,42 @@ html = driver.page_source
 
 
 # Parse HTML
-parsed = BeautifulSoup(html, "html.parser")
+parser = BeautifulSoup(html, "html.parser")
 
-# # Find all <div>, <span>, or <table> elements that may contain menu data
-# for tag in ["div", "span", "table"]:
-#     elements = parsed.find_all(tag)
-#     print(f"Found {len(elements)} {tag} elements.")
-#     for el in elements[:5]:  # Print first 5 elements to check
-#         print(el.text.strip())
+# with open("output.txt", "w", encoding="utf-8") as f:
+#     f.write(str(html))
+    
+item_re = re.compile(r'<div class="shortmenurecipes"><span.*?>(.*?)</span></div>',re.S)
+category_re = re.compile(r'<div class="shortmenucats"><span.*?>-- (.*?) --</span></div>',re.S)
+meal_time_re = re.compile(r'<div class="shortmenumeals">(.*?)</div>',re.S)
 
-import re
-rex = re.compile(r'<span.*?>(.*?)</span>',re.S|re.M)
-...
-data = """ <div class="shortmenurecipes"><span style="color: #000000">Donut Holes&nbsp;</span></div>"""
+# menu_items = [match.replace("&nbsp;", " ").strip() for match in item_re.findall(html)]
+# category_items = [match.strip() for match in category_re.findall(html)]
 
-span_items = [match.replace("&nbsp;", " ").strip() for match in rex.findall(html)]
-print(span_items)
-
-with open("output.txt", "w", encoding="utf-8") as f:
-    for item in span_items:
-        f.write(item + "\n")
-
+meal_time = None
+category = None
+menu_items = []
+with open("menu-items.json", "w", encoding="utf-8") as f:
+    for row in parser.find_all('div'):
+        row_str = str(row)
+        item_match = item_re.search(row_str)
+        cat_match = category_re.search(row_str)
+        meal_match = meal_time_re.search(row_str)
+        if meal_match:
+            meal_time = str(meal_match.groups()[0].strip())
+            # f.write(str(meal_match.groups()[0].strip()) + "\n")
+        if cat_match:
+            category = str(cat_match.groups()[0].strip())
+            # f.write(str(cat_match.groups()[0].strip()) + "\n")
+        if item_match:
+            item = {
+                "name": str(item_match.groups()[0].strip()),
+                "meal_time": meal_time,
+                "category": category,
+            }
+            menu_items.append(item)
+            # f.write(str(item_match.groups()[0].strip()) + "\n")
+    json.dump(menu_items, f, ensure_ascii=False, indent=4)
+        
 # Close browser
 driver.quit()
